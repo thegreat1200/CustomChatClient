be4be9da46782e83e79238498fafe9c08f837c2e
thegreat1200 <junk_mail000@icloud.com>
2015-08-01 07:59:52 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 (function(){
-
+  var ccc = {
+    room: {
+      name: $("#room-name > .bar-value").text(),
+      host: $("#room-host > .username").text(),
+      wel: $(".cm > .msg > div").eq(1).text()
+      if (wel.search("@BOT=")) {
+        API.chatLog("Found Bot!");
+      }
+    },
+    
+    start: function() {
+      API.on(API.CHAT, ccc.chatCheck);
+      $(".cm > .msg > div").eq(1).text();
+      
+      API.chatLog("CustomChat is now Loaded.");
+    }
+  };
 }).call(this);
